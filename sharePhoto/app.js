903d130ab0764befbb932055f5bd1746386f32d7
1kivanc <kivanc0556@gmail.com>
2023-03-16 17:56:55 +0300

    install template engine

--- sharePhoto/app.js
+++ sharePhoto/app.js
@@ -2,13 +2,46 @@ import express from 'express';
 
 const app = express();
 
+//template engine 
+
+app.set('view engine', 'ejs');
+
+//public 
+app.use(express.static('public'));
+
+
 const port = 3000;
 
+
+
 app.get('/',(req,res)=> {
-    res.send('index sayfası');
+    res.render('index');
+
 })
 
+app.get('/about',(req,res) => {
+    res.render('about');
+})
 
+app.get('/blog',(req,res) => {
+    res.render('blog');
+})
+
+app.get('/contact',(req,res) => {
+    res.render('contact');
+})
+
+app.get('/gallery',(req,res) => {
+    res.render('gallery');
+})
+
+app.get('/projects',(req,res) => {
+    res.render('projects');
+})
+
+app.get('/services',(req,res) => {
+    res.render('services');
+})
 
 
 
